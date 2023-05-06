c54e4a800187e846d988506bbcfea6f7dabdf473
boularakSaraManel <63054249+boularakSaraManel@users.noreply.github.com>
2023-05-06 18:42:50 +0100

    2nd commit: auth filter added

---
+++ src/main/java/com/example/servicegateway/util/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.example.servicegateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "357538782F413F4428472B4B6250655367566B59703373367639792442264529"; //same as the one in SECURITY-SERVICE
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
